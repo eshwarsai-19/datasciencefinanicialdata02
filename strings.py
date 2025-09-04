89a82c1909e9e555ca903c53f3d9d403458c6649
Eshwar sai <eshwarsai.b19@gmail.com>
2025-09-04 17:36:04 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+# today practice on strings
+
+d="dwaraka"
+D="dharshini"
+print("the complete name is: ",d +D)
+print("Nagesh","Shashi kala","Eshwar sai","Dwaraka Dharshini",sep="-")
+print("Nagesh","Shashi kala","Eshwar sai","Dwaraka Dharshini",sep="\t")
+print("Nagesh","Shashi kala","Eshwar sai","Dwaraka Dharshini",sep="\n")
+
+
+
