c2aadbb930c2b58fcc7b5283c39da8a1a0d1f4e1
Eshwar sai <eshwarsai.b19@gmail.com>
2025-09-05 18:02:15 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,10 @@
+# doing practice of iterative of while example
+
+n=int(input("enter the value of n:"))
+i=0
+while i<=n:
+    print(i)                       #line wise
+    print(i,end=" ")          #step by step
+    i=i+1
+    
+
